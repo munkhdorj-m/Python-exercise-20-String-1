63c193ed251017bb92b428f4e63cc2c153f292e5
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2024-12-17 09:10:02 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,53 +1,49 @@
 import pytest
 import inspect
-from assignment import print_love_python, sum_of_n_numbers, sum_of_digits, count_digits, print_from_five_to
+from assignment import count_vowels, is_palindrome, remove_spaces, count_word_frequencies
 
 def check_contains_loop(function):
     source = inspect.getsource(function)
     return 'for' in source or 'while' in source
-    
-def test1(capsys):
-    print_love_python()
-    captured = capsys.readouterr()
-    assert captured.out.strip() == "\n".join(["I Love Python"] * 10)
-    assert check_contains_loop(print_love_python)
 
 @pytest.mark.parametrize("input, expected", [
-    (5, 15),
-    (11, 66),
-    (3, 6),
-    (0, 0)
+    ("Hello World", 3),
+    ("Python Programming", 4),
+    ("BCDFG", 0),
+    ("AEIOUaeiou", 10),
+    ("", 0)
+])
+def test1(input, expected):
+    assert count_vowels(input) == expected
+    assert check_contains_loop(count_vowels)
+
+@pytest.mark.parametrize("input, expected", [
+    ("madam", True),
+    ("racecar", True),
+    ("hello", False),
+    ("Aibohphobia", True),
+    ("", True)
 ])
 def test2(input, expected):
-    assert sum_of_n_numbers(input) == expected
-    assert check_contains_loop(sum_of_n_numbers)
+    assert is_palindrome(input) == expected
 
 @pytest.mark.parametrize("input, expected", [
-    (125, 8),
-    (5, 5),
-    (1234, 10),
-    (0, 0)
+    ("Hello World", "HelloWorld"),
+    ("Python Programming Language", "PythonProgrammingLanguage"),
+    ("  Remove   spaces  ", "Removespaces"),
+    ("", ""),
+    ("NoSpaces", "NoSpaces")
 ])
 def test3(input, expected):
-    assert sum_of_digits(input) == expected
-    assert check_contains_loop(sum_of_digits)
+    assert remove_spaces(input) == expected
 
 @pytest.mark.parametrize("input, expected", [
-    (123, 3),
-    (9, 1),
-    (56741, 5),
+    ("hello hello world", {"hello": 2, "world": 1}),
+    ("this is a test this is", {"this": 2, "is": 2, "a": 1, "test": 1}),
+    ("count count count", {"count": 3}),
+    ("", {}),
+    ("word", {"word": 1})
 ])
 def test4(input, expected):
-    assert count_digits(input) == expected
-    assert check_contains_loop(count_digits)
-
-@pytest.mark.parametrize("input, expected", [
-    (9, [5, 6, 7, 8, 9]),
-    (7, [5, 6, 7]),
-    (-2, [5, 4, 3, 2, 1, 0, -1, -2])
-])
-def test5(capsys, input, expected):
-    print_from_five_to(input)
-    captured = capsys.readouterr()
-    assert captured.out.strip().splitlines() == list(map(str, expected))
-    assert check_contains_loop(print_from_five_to)
+    assert count_word_frequencies(input) == expected
+    assert check_contains_loop(count_word_frequencies)
